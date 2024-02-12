fbb54c5ce3922cc6a16a62e2c8d8f6647585f601
Karim Naqvi <naqvik@uregina.ca>
2024-02-12 14:55:21 -0600

    Bare f103rb proj with no Keil stuff

---
+++ code/app/simple.cpp
@@ -0,0 +1,14 @@
+/** A simple app, to demonstrate freertos */
+
+/* standard includes */
+
+/* freertos includes */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* HW-specific includes (move to bsp area) */
+#include "stm32f10x.h"
+
+int main() {
+    
+}
