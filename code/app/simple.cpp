16d051d2a306543137f0e18423a0a099e64228a3
Karim Naqvi <naqvik@uregina.ca>
2024-02-26 14:11:34 -0600

    Remove old, commented lines (busy-wait)

--- code/app/simple.cpp
+++ code/app/simple.cpp
@@ -94,12 +94,10 @@ void gpio_pin_onoff(GPIO_TypeDef* base, uint32_t pin, bool on) {
         // turn on PA5 LED
         gpio_pin_onoff(GPIOA, 5, 1);
         vTaskDelay(450);
-        //for (int volatile counter = 0; counter < 1000000; ++counter) { }
 
         // turn off PA5 LED
         gpio_pin_onoff(GPIOA, 5, 0);
         vTaskDelay(450);
-        //for (int volatile counter = 0; counter < 1000000; ++counter) { }
     }
     //return 0;
 }
@@ -114,13 +112,11 @@ void gpio_pin_onoff(GPIO_TypeDef* base, uint32_t pin, bool on) {
     while (1) {
         // turn on PA8 LED
         gpio_pin_onoff(GPIOA, 8, 1);
-        //for (int volatile counter = 0; counter < 1000000; ++counter) { }
         vTaskDelay(250);
 
         // turn off PA8 LED
         gpio_pin_onoff(GPIOA, 8, 0);
         vTaskDelay(250);
-        //for (int volatile counter = 0; counter < 1000000; ++counter) { }
     }
     //return 0;
 }
