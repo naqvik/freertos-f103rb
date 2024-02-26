350f3eee96c22b7a9f1ca9eb0f9d15bb176fb97f
Karim Naqvi <naqvik@uregina.ca>
2024-02-26 14:03:42 -0600

    Use gpio_pin_onoff() for both blinkers

--- code/app/simple.cpp
+++ code/app/simple.cpp
@@ -92,11 +92,13 @@ void gpio_pin_onoff(GPIO_TypeDef* base, uint32_t pin, bool on) {
 
     while (1) {
         // turn on PA5 LED
-        *((uint32_t volatile *)(0x40010800 + 0xc)) |=  1u<<5;
+        //*((uint32_t volatile *)(0x40010800 + 0xc)) |=  1u<<5;
+        gpio_pin_onoff(GPIOA, 5, 1);
         for (int volatile counter = 0; counter < 1000000; ++counter) { }
 
         // turn off PA5 LED
-        *((uint32_t volatile *)(0x40010800 + 0xc)) &= ~(1u<<5);
+        //*((uint32_t volatile *)(0x40010800 + 0xc)) &= ~(1u<<5);
+        gpio_pin_onoff(GPIOA, 5, 0);
         for (int volatile counter = 0; counter < 1000000; ++counter) { }
     }
     //return 0;
@@ -112,11 +114,13 @@ void gpio_pin_onoff(GPIO_TypeDef* base, uint32_t pin, bool on) {
 
     while (1) {
         // turn on PA8 LED
-        *((uint32_t volatile *)(0x40010800 + 0xc)) |=  1u<<8;
+        gpio_pin_onoff(GPIOA, 8, 1);
+        //*((uint32_t volatile *)(0x40010800 + 0xc)) |=  1u<<8;
         for (int volatile counter = 0; counter < 1000000; ++counter) { }
 
         // turn off PA8 LED
-        *((uint32_t volatile *)(0x40010800 + 0xc)) &= ~(1u<<8);
+        gpio_pin_onoff(GPIOA, 8, 0);
+        //*((uint32_t volatile *)(0x40010800 + 0xc)) &= ~(1u<<8);
         for (int volatile counter = 0; counter < 1000000; ++counter) { }
     }
     //return 0;
