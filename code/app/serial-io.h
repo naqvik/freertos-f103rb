f10763a9a193c53dcfa65ee5a17e9dbe08e9d4b4
Karim Naqvi <naqvik@uregina.ca>
2024-03-13 12:29:32 -0600

    Update file header comment

--- code/app/serial-io.h
+++ code/app/serial-io.h
@@ -1,5 +1,8 @@
 /**
-   Serial I/O.  For Keil we can just implement fputc and fgetc
+   Serial I/O.  For Keil we can just implement fputc and fgetc (in serial-io.c)
+   and clients will get the prototypes by including stdio.h.
+
+   Note: this implementation depends on MicroLib being enabled (which means no C++).
  */
 #ifndef SERIAL_IO_H
 #define SERIAL_IO_H
